de9dd62269ba786079dc6eaf2b70616bd0266723
Darbaz Ali <darbaz.a.h@gmail.com>
2020-06-29 13:09:21 +0300

    create Calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-
+import Calculator from './Calculator'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Calculator />
     </div>
   );
 };
---
+++ src/Calculator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div>
+                <h1>React Calc</h1>
+            </div>
+        )
+    }
+}
